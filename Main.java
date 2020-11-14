f2543cc4bfaeb90549c095b02760587ecb0d3164
AGuerrero20 <alexander.guerrero@patelhs.org>
2020-11-14 04:24:03 +0000

    most of it

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int [] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    String theNames[] = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
+    for(int i = 0; i < 15; i++){
+    
+    }
   }
 }
