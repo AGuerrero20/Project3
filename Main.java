cdfced8c34702334a5310527e4014ae23da0018d
AGuerrero20 <alexander.guerrero@patelhs.org>
2020-11-18 00:45:30 +0000

    found out how to receive the name and assign it to each element of the String array. Used a while loop to print all of the Student Names with their corresponding seats.

--- Main.java
+++ Main.java
@@ -5,8 +5,17 @@ class Main {
     int [] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
     String[] studentNames = new String[15];
     for(int i = 0; i < 15; i++){
+    Scanner scan = new Scanner(System.in);
+    
     System.out.println("What is the student's name?");
-    studentNames[i] = scan.next();
+    String name = scan.next();
+    studentNames[(i)] = (name);
+
+    }
+    int a = 0;
+    while(a < 15){
+      System.out.println(studentNames[a] + " sits in seat " + seats[a]);
+      a++;
 
     }
   }
